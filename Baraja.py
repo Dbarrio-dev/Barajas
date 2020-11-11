d6918dabf0109ad63b5a4b0b99a4424e8958d6a7
Daniel <d.barriele@gmail.com>
2020-11-11 22:32:49 +0100

    Funcion Baraja

--- Baraja.py
+++ Baraja.py
@@ -1,16 +1,21 @@
 palos = ['o', 'c', 'e', 'b']
 numeros = ['A', '2', '3', '4', '5', '6', '7', 'S', 'C', 'R']
 
-baraja =[]
 
 def crearBaraja():
 
+    baraja =[]
     for palo in palos:
         for numero in numeros:
             baraja.append(numero+palo)
     return baraja   
 
 
-crearBaraja()
+def barajar(lista_de_naipes)
+    for i in range (len(lista_de_naipes)):
+        nueva_pos = ramdom.radnrange(len(lista_de_naipes))
+        aux = lista_de_naipes[nueva_pos]
+        lista_de_naipes[nueva_pos] = lista_de_naipes[i]
+        lista_de_naipes [i] = aux
+    
 
-print (baraja)
